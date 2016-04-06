d696f0ead834610db184e2919d3bb9d6778ee38c
Jane Zhang <jane.zhang@hujiang.com>
2016-04-06 15:56:30 +0800

    template

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # HelloRxJS
 
+babel-loader
 ES2016转译器Babel入门指南:http://www.ruanyifeng.com/blog/2016/01/babel.html
 webpack入门指南:http://webpack.github.io/docs/tutorials/getting-started/
+webpack-dev-server
 
 
 #目录结构
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
-var path = require('path');
+var path = require('path'),
+  HtmlwebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  context: path.join(__dirname, 'src'),
   entry: "./index",
   output: {
-    path: __dirname,
+    path: path.join(__dirname, 'dist'),
     filename: "bundle.js"
   },
   module: {
     loaders: [
       {test: /\.js$/, loaders: ['babel'], include: path.join(__dirname, 'src')}
     ]
-  }
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      template: './index.html'
+    })
+  ]
 };
